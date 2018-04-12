e3ae4a1f7df59f49a5e55ca1eec71c8eb295f2cb
petitlapin86 <skinnerpa86@yahoo.com>
2018-04-12 11:35:37 -0400

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,26 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    begin
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req| #We use Faraday.get(url) to make a request to the API endpoint.
+        req.params['client_id'] = 'SBTKDLCAHKK0DIVCYECTEIFBLC1IW2UCUDUBI124PPUI3FAD'
+ #We know we need to set some params from our tests in Postman,
+        req.params['client_secret'] = '000O2IAVU4DCEUERHPUBZERQRQZJVLQR552AQO00Z3RXDOCB'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+        
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search' #To keep it simple, we're just going to render the search template again with the result.
   end
 end
